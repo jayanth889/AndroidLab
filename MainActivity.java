492cf08847f5ed981969bc43873c63e9edc6a820
jayanth889 <87356487+jayanth889@users.noreply.github.com>
2022-05-30 11:23:58 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,41 +1,40 @@
-package com.example.myapplication1;
+package com.example.contextmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Lifecycle;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Lifecycle","onCreate invoked");
-    }
-    protected void onStart(){
-        super.onStart();
-        Log.d("Lifecycle","onStart invoked");
-    }
-    protected void onResume(){
-        super.onResume();
-        Log.d("Lifecycle","onResume invoked");
-    }
-    protected void onPause(){
-        super.onPause();
-        Log.d("Lifecycle","onPause invoked");
-    }
-    protected void onStop(){
-        super.onStop();
-        Log.d("Lifecycle","onStop invoked");
-    }
-    protected void onRestart(){
-        super.onRestart();
-        Log.d("Lifecycle","onRestart invoked");
-    }
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.d("Lifecycle","onDestroy invoked");
+
+        button = findViewById(R.id.button);
+        registerForContextMenu(button);
+
+
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, ""+item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onContextItemSelected(item);
+    }
 }
